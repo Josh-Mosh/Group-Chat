a68da615031a9f48d3762c3253d012c987d600a9
Josh Gillespie <Joshua.Neal.Gillespie@gmail.com>
2014-02-28 09:13:52 -0800

    added nodejs room functionality

--- routes/index.js
+++ routes/index.js
@@ -5,51 +5,95 @@ module.exports = function Route(app){
 
   users = [];
   messages = [];
+  rooms = [];
+  users['general'] = [];
+  messages['general'] = [];
+  users['programmers'] = [];
+  messages['programmers'] = [];
+  users['artists'] = [];
+  messages['artists'] = [];
+  users['athletes'] = [];
+  messages['athletes'] = [];
+  users['plummers'] = [];
+  messages['plummers'] = [];
 
   app.io.route('new_user', function(req){
 
-    var USER = function(name) {
+    var User = function(name) {
       this.name = name;
       this.id = undefined;
+      this.current_room = undefined;
     }
 
-    user = new USER(req.data.name);
+    user = new User(req.data.name);
 
     user.id = req.sessionID;
 
-    users.push(user);
+    /// Join specified room ///
+    current_room = req.data.room;
 
-    req.io.broadcast('show_new_user', user);
+    user.current_room = current_room;
 
-    req.io.emit('existing_users', users);
+    req.io.join(current_room);
 
-    req.io.emit('existing_messages', messages);
+    users[current_room].push(user);
 
+    // console.log(users);
+    // users.push(user);
+
+    /// Show new user in room list ///
+    req.io.room(current_room).broadcast('show_new_user', user);
+
+    /// Show existing users in room to new user ///
+    req.io.emit('existing_users', users[current_room]);
+
+    /// Show existing messages in room to new user ///
+    req.io.emit('existing_messages', messages[current_room]);
   })
 
   app.io.route('new_message', function(req){
 
+    user_room = req.data.room;
     content = req.data.message;
     name = req.data.name;
     message = {message: content, name: name};
-    messages.push(message);
 
-    app.io.broadcast('updated_message', message);
+    messages[user_room].push(message);
+
+    app.io.room(user_room).broadcast('updated_message', message);
   })
 
   app.io.route('disconnect', function(req){
-    for(var i=0; i < users.length; i++ )
+    // console.log('##########Disconnected#########', req);
+
+    console.log('####before', users);
+
+    for(var prop in users )
     {
-      if(users[i].id === req.sessionID)
+      room = users[prop];
+      // console.log('users[prop]', users[prop])
+      for(var user in room)
       {
-        name = users[i].name;
-        users.splice(i, 1);
+        // console.log(room[user].id);
+        if(room[user].id == req.sessionID)
+        {
+          disconnected_name = room[user].name
+          disconnected_user = {id: room[user].id, name: disconnected_name}
+          console.log('indexOf', users.indexOf(users[prop][user]));
+          var splice_index = users[prop].indexOf(room[user]);
+          // console.log('users[prop][user]', users[prop][user]);
+          // console.log('users[prop]', users[prop]);
+
+          req.io.room(users[prop][user].current_room).broadcast('disconnect_user', disconnected_user )
+
+          users[prop].splice(splice_index, 1);
+
+        }
       }
     }
-
-    disconnected_user = {id: req.sessionID, name: name}
-    // console.log('##########Disconnected#########', req);
-    req.io.broadcast('disconnect_user', disconnected_user )
+    console.log('####after', users);
+    
+    
   });
 
 
